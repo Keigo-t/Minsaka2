cd08e258d58e34b2141016d1a080536c91d390a1
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2023-04-27 14:03:15 +0000

    [fix]admin_user_show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,16 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :is_deleted)
+  end
+
 end
