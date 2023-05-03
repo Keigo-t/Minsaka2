a8399cf65e8eeb81680d36a3426b8d42442792d7
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2023-05-03 14:04:50 +0000

    [fix]user_comment

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,4 +8,10 @@ class Admin::PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    redirect_to admin_post_path(@post.id)
+  end
+
 end
--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,15 @@
 class Public::CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.save
+    redirect_to posts_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:commnet).permit(:comment, :post_id)
+  end
+
 end
